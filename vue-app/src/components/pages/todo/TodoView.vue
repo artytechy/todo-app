792132986810d9db0e4f8855a464535900e624c1
artytechy <arturs.ribakovs@outlook.com>
2025-02-16 11:51:02 +0000

    make reset button functional

--- vue-app/src/components/pages/todo/TodoView.vue
+++ vue-app/src/components/pages/todo/TodoView.vue
@@ -99,7 +99,7 @@
       <span class="panel-block"> No todos found. </span>
     </template>
     <div class="panel-block">
-      <button class="button is-link is-outlined is-fullwidth">
+      <button @click="setFilter(0)" class="button is-link is-outlined is-fullwidth">
         Reset all filters
       </button>
     </div>
